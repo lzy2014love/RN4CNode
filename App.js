4298eec19d475f6fc11829d1d9c873363816636f
lzy <1012938458@qq.com>
2017-11-28 23:53:47 +0800

    chore(docs): add .eslintrc.js , editorconfig

--- App.js
+++ App.js
@@ -4,36 +4,36 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   Platform,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+    'Shake or press menu button for dev menu'
+})
 
-export default class App extends Component<{}> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native!sd
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit App.js
+          To get started, edit App.jshaha
         </Text>
         <Text style={styles.instructions}>
           {instructions}
         </Text>
       </View>
-    );
+    )
   }
 }
 
@@ -42,16 +42,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginBottom: 5
+  }
+})
