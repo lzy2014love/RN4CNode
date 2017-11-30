e04a12fd582cc6624fc8b78304c8b1ad73ab8f15
lzy <1012938458@qq.com>
2017-11-30 22:19:00 +0800

    chore: add jaconfig.json

--- App.js
+++ App.js
@@ -1,57 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+/*
+ * @Author: lzy
+ * @Date: 2017-11-30 00:33:51
+ * @Last Modified by: lzy
+ * @Last Modified time: 2017-11-30 00:34:28
  */
-
 import React, { Component } from 'react'
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native'
+import SC from 'styled-components/native'
+import { Platform } from 'react-native'
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
+  ios: `Press Cmd+R to reload,
+  Cmd+D or shake for dev menu`,
+  android: `Double tap R on your keyboard to reload,
+  Shake or press menu button for dev menu`
 })
+
+const SCView = SC.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #ff0;
+`
+const SCTextWelcome = SC.Text`
+  font-size: 35px;
+  text-align: center;
+  margin: 10px;
+`
+const SCTextInstructions = SC.Text`
+  text-align: center;
+  color: #33e;
+  margin-bottom: 50px;
+  color: #fff;
+
+`
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!sd
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.jshaha
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <SCView>
+        <SCTextWelcome>Welcome to React Native!</SCTextWelcome>
+        <SCTextInstructions>To get started, edit App.jshaha</SCTextInstructions>
+        <SCTextInstructions>{instructions}</SCTextInstructions>
+      </SCView>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-})
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: lzy
+ * @Date: 2017-11-30 00:34:13
+ * @Last Modified by:   lzy
+ * @Last Modified time: 2017-11-30 00:34:13
+ */
 import { AppRegistry } from 'react-native'
 import App from './App'
 
